9164d2141feb4622edf762714f0fb08219c87245
zhzhwcn <zhzhwmail@gmail.com>
2020-06-04 21:34:20 +0800

    add OpenFeignConfig

---
+++ OpenFeign.net/OpenFeignConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenFeign.net.Retry;
+
+namespace OpenFeign.net
+{
+    public class OpenFeignConfig
+    {
+        public IRetry Retry { get; set; }
+    }
+}
